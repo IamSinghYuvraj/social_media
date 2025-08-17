09f58335b581ca44d6665965c573196bb43140d4
Yuvraj Singh <121106713+IamSinghYuvraj@users.noreply.github.com>
2025-08-17 17:07:01 +0530

    production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,17 +54,4 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('daisyui'),
-  ],
-  daisyui: {
-    themes: ['light', 'dark'],
-    darkTheme: 'dark',
-    base: true,
-    styled: true,
-    utils: true,
-    prefix: '',
-    logs: true,
-    themeRoot: ':root',
-  },
 }
